5ce2658ffef305b49ba5845f5e1b9c3de337f427
Takeshi Yamamuro <yamamuro@apache.org>
2017-03-26 23:57:47 +0900

    Add helps in start-sql-sever.sh

--- sbin/start-sql-server.sh
+++ sbin/start-sql-server.sh
@@ -37,20 +37,14 @@ else
   SPARK_DIR=${SPARK_HOME}
 fi
 
-# An entry point for the SQL server
-CLASS="org.apache.spark.sql.server.SQLServer"
-
 function usage {
   echo "Usage: ./sbin/start-sql-server.sh [options] [SQL server options]"
-  pattern="usage"
-  pattern+="\|Spark Command: "
-  pattern+="\|======="
-  pattern+="\|--help"
-
   "${SPARK_DIR}"/bin/spark-submit --help 2>&1 | grep -v Usage 1>&2
-  echo
   echo "SQL server options:"
-  "${SPARK_DIR}"/bin/spark-class $CLASS --help 2>&1 | grep -v "$pattern" 1>&2
+  echo "  --conf spark.sql.server.port=NUM            Port number of SQL server interface (Default: 5432)."
+  echo "  --conf spark.sql.server.worker.threads=NUM  # of SQLServer worker threads (Default: 4)."
+  echo "  --conf spark.sql.server.ssl.enabled=BOOL    Enable SSL encryption (Default: false)."
+  echo
 }
 
 if [[ "$@" = *--help ]] || [[ "$@" = *-h ]]; then
@@ -58,20 +52,23 @@ if [[ "$@" = *--help ]] || [[ "$@" = *-h ]]; then
   exit 0
 fi
 
-export SUBMIT_USAGE_FUNCTION=usage
-
 # Resolve a jar location for the SQL server
 _SPARK_VERSION=`grep "<spark.version>" "${_DIR}/../pom.xml" | head -n1 | awk -F '[<>]' '{print $3}'`
 _SCALA_VERSION=`grep "<scala.binary.version>" "${_DIR}/../pom.xml" | head -n1 | awk -F '[<>]' '{print $3}'`
 _JAR_FILE="sql-server_${_SCALA_VERSION}-${_SPARK_VERSION}-SNAPSHOT-with-dependencies.jar"
 _BUILT_JAR="$_DIR/../target/${_JAR_FILE}"
 if [ -e $_BUILT_JAR ]; then
-  _JAR=$_BUILT_JAR
+  RESOURCES=$_BUILT_JAR
 else
-  _JAR="$_DIR/../assembly/${_JAR_FILE}"
-  echo "${_BUILT_JAR} not found, so use pre-compiled ${_JAR}"
+  RESOURCES="$_DIR/../assembly/${_JAR_FILE}"
+  echo "${_BUILT_JAR} not found, so use pre-compiled ${RESOURCES}"
 fi
 
 echo "Using \`spark-submit\` from path: $SPARK_DIR" 1>&2
 
-exec "${SPARK_DIR}"/sbin/spark-daemon.sh submit $CLASS 1 --name "Spark SQL Server" --properties-file ${_DIR}/../conf/spark-defaults.conf "$@" ${_JAR}
+# An entry point for the SQL server
+CLASS="org.apache.spark.sql.server.SQLServer"
+PROPERTY_FILE=${_DIR}/../conf/spark-defaults.conf
+APP_NAME="Spark SQL Server"
+
+exec "${SPARK_DIR}"/sbin/spark-daemon.sh submit $CLASS 1 --name "${APP_NAME}" --properties-file ${PROPERTY_FILE} "$@" ${RESOURCES}
