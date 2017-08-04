91facddec3485ed8b29cddc1d3ff9d05c306549b
Takeshi Yamamuro <yamamuro@apache.org>
2017-08-04 12:04:41 +0900

    Support nanosecs in binary Timestamp

--- sql/sql-server/src/main/scala/org/apache/spark/sql/server/service/postgresql/protocol/v3/PostgreSQLRowConverters.scala
+++ sql/sql-server/src/main/scala/org/apache/spark/sql/server/service/postgresql/protocol/v3/PostgreSQLRowConverters.scala
@@ -245,7 +245,8 @@ private class TimestampColumnBinaryWriter(ordinal: Int, conf: SQLConf)
   override def nullSafeWriter(row: InternalRow, byteBuffer: ByteBuffer): Unit = {
     val timestamp = DateTimeUtils.toJavaTimestamp(row.getLong(ordinal))
     val millis = timestamp.getTime + timezone.getRawOffset
-    val pgTime = toPgSecs(millis / 1000) * 1000000L
+    val nanos = timestamp.getNanos / 1000
+    val pgTime = toPgSecs(millis / 1000) * 1000000L + nanos
     byteBuffer.putInt(8)
     byteBuffer.putLong(pgTime)
   }
